06d776852fdf41bbb77dcdd586b2ce4a0b5b0989
RozWelch <rozwelch@gmail.com>
2023-04-14 15:45:12 +0000

    Added products app and models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, SkinCategory
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(SkinCategory)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class SkinCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('SkinCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    product_size = models.CharField(max_length=60)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    is_new = models.BooleanField(default=True)
+    star_rating = models.IntegerField(null=True, blank=True)
+    review_count = models.IntegerField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
