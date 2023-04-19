56000f484083fe16f0a21a88314f541135da3902
RozWelch <rozwelch@gmail.com>
2023-04-19 07:59:24 +0000

    Deploying to heroku - part 1

--- pure_glow/settings.py
+++ pure_glow/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
@@ -124,11 +126,11 @@ WSGI_APPLICATION = 'pure_glow.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
