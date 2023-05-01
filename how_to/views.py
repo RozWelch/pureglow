2ff41d5635224658f0e5dafe48418280f9233d97
RozWelch <rozwelch@gmail.com>
2023-05-01 16:33:34 +0000

    Added views for how to articles and html templates

--- how_to/views.py
+++ how_to/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import HowTo, ArticleComment
 
-# Create your views here.
+
+class HowToList (generic.ListView):
+    model = HowTo
+    queryset = order_by('-created_on')
+    template_name = articles.html
+    paginate_by = 6
+    
