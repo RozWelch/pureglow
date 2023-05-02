067ce261dbe3f48f703e99fa68a084ac80648d63
RozWelch <rozwelch@gmail.com>
2023-05-02 11:18:10 +0000

    Added how to article details view

--- how_to/views.py
+++ how_to/views.py
@@ -9,21 +9,13 @@ class HowToList(generic.ListView):
     paginate_by = 6
 
 
-class HowToDetail(View):
+def howto_detail(request, product_id):
+    """ A view to show full how to article """
 
-    def get(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+    article = get_object_or_404(HowTo, slug=slug)
 
-        return render(
-            request,
-            "articles_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked
-            },
-        )
+    context = {
+        'howto': howto,
+    }
+
+    return render(request, 'how_to/articles_detail.html', context)
