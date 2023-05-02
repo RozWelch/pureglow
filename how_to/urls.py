f3a59360335a73b922c15de24b840b06767895d7
RozWelch <rozwelch@gmail.com>
2023-05-02 11:05:49 +0000

    Update requirements file - corrected summernote version

--- how_to/urls.py
+++ how_to/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.HowToList.as_view(), name="skincare_articles"),
+    path('<int:howto_id>/', views.howto_detail, name='howto_detail'),
 ]
