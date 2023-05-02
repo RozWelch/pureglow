e45726a977ca980255db1404ae324bad38c2b302
RozWelch <rozwelch@gmail.com>
2023-05-02 11:06:51 +0000

    Updated requirements file with correct summernote version

--- how_to/views.py
+++ how_to/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import HowTo, ArticleComment
 
 
-class HowToList (generic.ListView):
+class HowToList(generic.ListView):
     model = HowTo
     template_name = "articles.html"
     paginate_by = 6
+
+
+class HowToDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "articles_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
