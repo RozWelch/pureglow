2dd219175901bd5c27d81d5634a69823e94b778e
RozWelch <rozwelch@gmail.com>
2023-05-03 13:00:53 +0000

    Fix how to view, correct link to article detail

--- how_to/urls.py
+++ how_to/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.HowToList.as_view(), name="skincare_articles"),
-    path('<slug:slug>/', views.HowToDetail.as_view(), name='howto_detail'),
+    path('<slug:slug>/', views.howto_detail, name='howto_detail'),
 ]
--- how_to/views.py
+++ how_to/views.py
@@ -8,22 +8,34 @@ class HowToList(generic.ListView):
     template_name = "articles.html"
 
 
-class HowToDetail(View):
+def howto_detail(request, slug):
+    """ A view to show how to articles """
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = HowTo.objects
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+    howto = get_object_or_404(HowTo, slug=slug)
 
-        return render(
-            request,
-            "articles_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked
-            },
-        )
+    context = {
+        'howto': howto,
+    }
+
+    return render(request, 'articles_detail.html', context)
+
+
+# class HowToDetail(View):
+
+#     def get(self, request, slug, *args, **kwargs):
+#         queryset = HowTo.objects
+#         post = get_object_or_404(queryset, slug=slug)
+#         comments = post.comments.filter(approved=True).order_by("-created_on")
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exists():
+#             liked = True
+
+#         return render(
+#             request,
+#             "articles_detail.html",
+#             {
+#                 "post": post,
+#                 "comments": comments,
+#                 "liked": liked
+#             },
+#         )
