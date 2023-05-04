142488797255d636939b027224600a24540ce17d
RozWelch <rozwelch@gmail.com>
2023-05-04 14:40:42 +0000

    Added safe tag to how to article body content -Fixed spacing on main nav bar -Darkened main text colour for accessibility -fixed colour on footer text heading

--- how_to/views.py
+++ how_to/views.py
@@ -19,23 +19,3 @@ def howto_detail(request, slug):
 
     return render(request, 'articles_detail.html', context)
 
-
-# class HowToDetail(View):
-
-#     def get(self, request, slug, *args, **kwargs):
-#         queryset = HowTo.objects
-#         post = get_object_or_404(queryset, slug=slug)
-#         comments = post.comments.filter(approved=True).order_by("-created_on")
-#         liked = False
-#         if post.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-
-#         return render(
-#             request,
-#             "articles_detail.html",
-#             {
-#                 "post": post,
-#                 "comments": comments,
-#                 "liked": liked
-#             },
-#         )
