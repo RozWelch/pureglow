edb2c634f5b33d7a84569b8bd055d82ecb0cef2f
RozWelch <rozwelch@gmail.com>
2023-05-26 09:51:19 +0000

    Fixed error on Search bar views file

--- products/views.py
+++ products/views.py
@@ -45,9 +45,7 @@ def all_products(request):
                                "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(
-                name__icontains=query) | Q(description__icontains=query) | Q(
-                excerpt__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)  # noqa
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
